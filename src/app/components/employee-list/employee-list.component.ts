3f97f2f7c965ea68e4970bb8478fc5b990e50e51
Lindos Mahadi <lindosmahadi@gmail.com>
2024-07-06 19:53:52 +0600

    All Data display

--- src/app/components/employee-list/employee-list.component.ts
+++ src/app/components/employee-list/employee-list.component.ts
@@ -1,18 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { IEmployee } from '../../interfaces/employee';
 import { HttpService } from '../../http.service';
+import {MatTable, MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatButtonModule, MatTable],
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
   employeeList: IEmployee[] = [];
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'title', 'address', 'image'];
 
-  httpService = inject(HttpService);
+  constructor(private httpService: HttpService, private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.httpService.getAllEmployee().subscribe(result => {
@@ -21,4 +25,9 @@ export class EmployeeListComponent implements OnInit {
       console.log("Hello");
     });
   }
+
+  getElementImageUrl(base64Image: string): string {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64,${base64Image}`) as string;
+  }
+  
 }
--- src/app/interfaces/employee.ts
+++ src/app/interfaces/employee.ts
@@ -1,9 +1,9 @@
-export interface IEmployee {
-    firstName: string;
-    lastName: string;
-    title: string;
-    address: string;
-    image: string;
-    id: number;
-  }
-  
+export interface IEmployee
+{
+     firstName: string,
+    lastName: string,
+    title: string,
+    address: string,
+    image: string,
+    id: number
+}
